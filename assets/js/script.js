9795d3181f0659940b4152475d23dd4c119cda3c
Jason A. Arter <jason.arter@gmail.com>
2023-01-09 21:43:17 -0800

    Basic functionality working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,41 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Globals.
+var currentTime = dayjs();
+var currentHour = currentTime.hour();
+var pageEl = $("#base");
+var schedule = {};
+var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
+
+// Function wrapped in JQuery to defer until html elements are loaded.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  for (var i = 0; i <= 23; i++) {
+    currentDivEl = $(`#hour-${i}`);
+    if (i < currentHour) {
+      currentDivEl.addClass("past");
+    } else if (i === currentHour) {
+      currentDivEl.addClass("present");
+    } else {
+      currentDivEl.addClass("future");
+    }
+  }
+
+
+  // listener for click events on the save button. 
+  pageEl.on('click', '.saveBtn', function (event) {
+    var buttonParentEl = $(this).parent();
+    var currentTextEl = $(buttonParentEl).children().eq(1);
+    var parentElId = $(buttonParentEl).attr("id");
+    enteredText = currentTextEl[0].value;
+    console.log(parentElId);
+    console.log(enteredText);
+    schedule[parentElId] = enteredText;
+    localStorage.setItem(schedule, JSON.stringify(schedule));
+  });
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  //
   //
   // TODO: Add code to display the current date in the header of the page.
 });
